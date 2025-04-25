7a511297d53b0c56338113da0d1ee6ec03ba6159
noahbustard <158092134+noahbustard@users.noreply.github.com>
2025-04-25 15:22:20 -0500

    Completed Section 5

--- src/servomotor.c
+++ src/servomotor.c
@@ -18,37 +18,83 @@
 #include "servomotor.h"
 #include "interrupt_support.h"
 
-#define SERVO_PIN           (22)
-#define PULSE_INCREMENT_uS  (INT32_MAX)
-#define SIGNAL_PERIOD_uS    (INT32_MAX)
+#define SERVO_PIN           (20)
+#define PULSE_INCREMENT_uS  (500000)
+#define SIGNAL_PERIOD_uS    (20000000)
 
 static int volatile pulse_width_us;
+static int volatile time_until_rising_edge;
+static int volatile time_until_falling_edge;
+static bool initialized = false;
+static bool toggled = false;
+static bool button_already_pressed = false;
+static bool centered = false;
 
-static void handle_timer_interrupt();
+static void handle_timer_interrupt() {
+    time_until_rising_edge -= PULSE_INCREMENT_uS;
+    time_until_falling_edge -= PULSE_INCREMENT_uS;
+
+    if (time_until_rising_edge <= 0) {
+        digitalWrite(SERVO_PIN, 1);
+        time_until_rising_edge += SIGNAL_PERIOD_uS;
+        time_until_falling_edge = pulse_width_us;
+    }
+
+    if (time_until_falling_edge <= 0) {
+        digitalWrite(SERVO_PIN, 0);
+    }
+}
 
 void initialize_servo() {
     cowpi_set_output_pins(1 << SERVO_PIN);
     center_servo();
-//    register_periodic_timer_ISR(0, PULSE_INCREMENT_uS, handle_timer_interrupt);
+    register_periodic_timer_ISR(0, PULSE_INCREMENT_uS, handle_timer_interrupt);
 }
 
 char *test_servo(char *buffer) {
-    ;
+
+    if (!initialized) {
+        center_servo();
+        initialized = true;
+        centered = true;
+        sprintf(buffer, "Center");
+        return buffer;
+    }
+
+    if (cowpi_right_button_is_pressed()) {
+        if (!button_already_pressed) {
+            toggled = !toggled;
+            button_already_pressed = true;
+            time_until_rising_edge = 0;
+            centered = false;
+        }
+    } else {
+        button_already_pressed = false;
+    }
+
+    if (centered) {
+        center_servo();
+        sprintf(buffer, "Center");
+    } else if (toggled) {
+        rotate_full_clockwise();
+        sprintf(buffer, "Clockwise");
+    } else {
+        rotate_full_counterclockwise();
+        sprintf(buffer, "Counterclockwise");
+    }
+
     return buffer;
 }
 
+
 void center_servo() {
-    ;
+    pulse_width_us = 1500000;
 }
 
 void rotate_full_clockwise() {
-    ;
+    pulse_width_us = 2000000;
 }
 
 void rotate_full_counterclockwise() {
-    ;
-}
-
-static void handle_timer_interrupt() {
-    ;
+    pulse_width_us = 1000000;
 }
