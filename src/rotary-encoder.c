f23766ac595944bba34ee046f57745f7475312ff
Caden France <160537486+cfrance3@users.noreply.github.com>
2025-04-27 21:05:37 -0500

    sections 3-5

--- src/rotary-encoder.c
+++ src/rotary-encoder.c
@@ -1,101 +1,175 @@
-/**************************************************************************//**
- *
- * @file rotary-encoder.c
- *
- * @author Noah Bustard
- * @author Caden France
- *
- * @brief Code to determine the direction that a rotary encoder is turning.
- *
- ******************************************************************************/
+/**************************************************************************/ /**
+                                                                              *
+                                                                              * @file rotary-encoder.c
+                                                                              *
+                                                                              * @author NOAH BUSTARD
+                                                                              * @author CADEN FRANCE
+                                                                              *
+                                                                              * @brief Code to determine the direction that a rotary encoder is turning.
+                                                                              *
+                                                                              ******************************************************************************/
 
 /*
  * ComboLock GroupLab assignment and starter code (c) 2022-24 Christopher A. Bohn
  * ComboLock solution (c) the above-named students
  */
 
- #include <CowPi.h>
- #include "interrupt_support.h"
- #include "rotary-encoder.h"
- 
- #define A_WIPER_PIN         (16)
- #define B_WIPER_PIN         (A_WIPER_PIN + 1)
- 
- typedef enum {
-     HIGH_HIGH, HIGH_LOW, LOW_LOW, LOW_HIGH, UNKNOWN
- } rotation_state_t;
- 
- static rotation_state_t volatile state;
- static direction_t volatile direction = STATIONARY;
- static int volatile clockwise_count = 0;
- static int volatile counterclockwise_count = 0;
- 
- static void handle_quadrature_interrupt();
- 
- void initialize_rotary_encoder() {
-     cowpi_set_pullup_input_pins((1 << A_WIPER_PIN) | (1 << B_WIPER_PIN));
-     register_pin_ISR((1 << A_WIPER_PIN) | (1 << B_WIPER_PIN), handle_quadrature_interrupt);
- 
-     uint8_t quadrature = get_quadrature();
-     switch (quadrature) {
-         case 0b11: state = HIGH_HIGH; break;
-         case 0b10: state = HIGH_LOW; break;
-         case 0b00: state = LOW_LOW; break;
-         case 0b01: state = LOW_HIGH; break;
-         default:   state = UNKNOWN; break;
-     }
-     
-     register_pin_ISR((1 << A_WIPER_PIN) | (1 << B_WIPER_PIN), handle_quadrature_interrupt);
-     
- }
- 
- uint8_t get_quadrature() {
- 
-     bool a = digitalRead(A_WIPER_PIN);
-     bool b = digitalRead(B_WIPER_PIN);
- 
-     return (b << 1) | a;
- }
- 
- 
- char *count_rotations(char *buffer) {
-     sprintf(buffer, "CW:%d CCW:%d", clockwise_count, counterclockwise_count);
-     return buffer;
- }
- 
- 
- direction_t get_direction() {
-     direction_t last_direction = direction;
-     direction = STATIONARY;        
-     return last_direction;
- }
- 
- 
- static void handle_quadrature_interrupt() {
-     static rotation_state_t last_state = UNKNOWN;
-     uint8_t quadrature = get_quadrature();
- 
-     rotation_state_t new_state;
-     switch (quadrature) {
-         case 0b11: new_state = HIGH_HIGH; break;
-         case 0b10: new_state = HIGH_LOW; break;
-         case 0b00: new_state = LOW_LOW; break;
-         case 0b01: new_state = LOW_HIGH; break;
-         default:   new_state = UNKNOWN; break;
-     }
- 
- 
-     if (new_state == LOW_LOW) {
-         if (last_state == HIGH_LOW) {
-             direction = CLOCKWISE;
-             clockwise_count++;
-         } else if (last_state == LOW_HIGH) {
-             direction = COUNTERCLOCKWISE;
-             counterclockwise_count++;
-         }
-     }
- 
-     last_state = state;
-     state = new_state;
- }
- 
+#include <CowPi.h>
+#include "interrupt_support.h"
+#include "rotary-encoder.h"
+
+#define A_WIPER_PIN (16)
+#define B_WIPER_PIN (A_WIPER_PIN + 1)
+
+typedef enum
+{
+    HIGH_HIGH,
+    HIGH_LOW,
+    LOW_LOW,
+    LOW_HIGH,
+    UNKNOWN
+} rotation_state_t;
+
+static rotation_state_t volatile state;
+static direction_t volatile direction = STATIONARY;
+static int volatile clockwise_count = 0;
+static int volatile counterclockwise_count = 0;
+
+static void handle_quadrature_interrupt();
+
+void initialize_rotary_encoder()
+{
+    cowpi_set_pullup_input_pins((1 << A_WIPER_PIN) | (1 << B_WIPER_PIN));
+    uint8_t quadrature = get_quadrature();
+    switch (quadrature)
+    {
+    case 0b11:
+        state = HIGH_HIGH;
+        break;
+    case 0b10:
+        state = HIGH_LOW;
+        break;
+    case 0b00:
+        state = LOW_LOW;
+        break;
+    case 0b01:
+        state = LOW_HIGH;
+        break;
+    default:
+        state = UNKNOWN;
+        break;
+    }
+
+    register_pin_ISR((1 << A_WIPER_PIN) | (1 << B_WIPER_PIN), handle_quadrature_interrupt);
+}
+
+uint8_t get_quadrature()
+{
+    int a = digitalRead(A_WIPER_PIN);
+    int b = digitalRead(B_WIPER_PIN);
+    return (b << 1) | a;
+}
+
+char *count_rotations(char *buffer)
+{
+    sprintf(buffer, "CW:%d CCW:%d", clockwise_count, counterclockwise_count);
+    return buffer;
+}
+
+direction_t get_direction()
+{
+    direction_t direction_copy = direction;
+    direction = STATIONARY;
+    return direction_copy;
+}
+
+rotation_state_t decode_quadrature(uint8_t quadrature)
+{
+    rotation_state_t decoded_state = UNKNOWN;
+    switch (quadrature)
+    {
+    case 0b00:
+        decoded_state = LOW_LOW;
+        break;
+    case 0b01:
+        decoded_state = LOW_HIGH;
+        break;
+    case 0b10:
+        decoded_state = HIGH_LOW;
+        break;
+    case 0b11:
+        decoded_state = HIGH_HIGH;
+        break;
+    }
+    return decoded_state;
+}
+
+static void handle_quadrature_interrupt()
+{
+    static rotation_state_t last_state = HIGH_HIGH;
+    uint8_t quadrature = get_quadrature();
+    rotation_state_t decoded_state = decode_quadrature(quadrature);
+    rotation_state_t new_state = state;
+
+    if (decoded_state == state)
+    {
+        return;
+    }
+
+    switch (state)
+    {
+    case LOW_LOW:
+        if (decoded_state == HIGH_LOW && last_state == LOW_HIGH)
+        {
+            new_state = HIGH_LOW;
+        }
+        else if (decoded_state == LOW_HIGH && last_state == HIGH_LOW)
+        {
+            new_state = LOW_HIGH;
+        }
+        break;
+    case LOW_HIGH:
+        if (decoded_state == HIGH_HIGH)
+        {
+            new_state = HIGH_HIGH;
+        }
+        else if (decoded_state == LOW_LOW && last_state == HIGH_HIGH)
+        {
+            new_state = LOW_LOW;
+            counterclockwise_count++;
+            direction = COUNTERCLOCKWISE;
+        }
+        break;
+    case HIGH_LOW:
+        if (decoded_state == HIGH_HIGH)
+        {
+            new_state = HIGH_HIGH;
+        }
+        else if (decoded_state == LOW_LOW && last_state == HIGH_HIGH)
+        {
+            new_state = LOW_LOW;
+            clockwise_count++;
+            direction = CLOCKWISE;
+        }
+        break;
+    case HIGH_HIGH:
+        if (decoded_state == LOW_HIGH)
+        {
+            new_state = LOW_HIGH;
+        }
+        else if (decoded_state == HIGH_LOW)
+        {
+            new_state = HIGH_LOW;
+        }
+        break;
+    case UNKNOWN:
+        new_state = decoded_state;
+        break;
+    }
+
+    if (new_state != state)
+    {
+        last_state = state;
+        state = new_state;
+    }
+}
--- src/servomotor.c
+++ src/servomotor.c
@@ -1,13 +1,13 @@
-/**************************************************************************//**
- *
- * @file rotary-encoder.c
- *
- * @author Noah Bustard
- * @author Caden France
- *
- * @brief Code to control a servomotor.
- *
- ******************************************************************************/
+/**************************************************************************/ /**
+                                                                              *
+                                                                              * @file rotary-encoder.c
+                                                                              *
+                                                                              * @author NOAH BUSTARD
+                                                                              * @author CADEN FRANCE
+                                                                              *
+                                                                              * @brief Code to control a servomotor.
+                                                                              *
+                                                                              ******************************************************************************/
 
 /*
  * ComboLock GroupLab assignment and starter code (c) 2022-24 Christopher A. Bohn
@@ -18,83 +18,78 @@
 #include "servomotor.h"
 #include "interrupt_support.h"
 
-#define SERVO_PIN           (22)
-#define PULSE_INCREMENT_uS  (500)
-#define SIGNAL_PERIOD_uS    (20000)
+#define SERVO_PIN (22)
+#define PULSE_INCREMENT_uS (500)
+#define SIGNAL_PERIOD_uS (20000)
 
 static int volatile pulse_width_us;
-static int volatile time_until_rising_edge;
-static int volatile time_until_falling_edge;
-static bool initialized = false;
-static bool toggled = false;
-static bool button_already_pressed = false;
-static bool centered = false;
+static int volatile time_until_rising_edge_us;
+static int volatile time_until_falling_edge_us;
 
-static void handle_timer_interrupt() {
-    time_until_rising_edge -= PULSE_INCREMENT_uS;
-    time_until_falling_edge -= PULSE_INCREMENT_uS;
+static void handle_timer_interrupt();
 
-    if (time_until_rising_edge <= 0) {
-        digitalWrite(SERVO_PIN, 1);
-        time_until_rising_edge += SIGNAL_PERIOD_uS;
-        time_until_falling_edge = pulse_width_us;
-    }
-
-    if (time_until_falling_edge <= 0) {
-        digitalWrite(SERVO_PIN, 0);
-    }
-}
-
-void initialize_servo() {
+void initialize_servo()
+{
     cowpi_set_output_pins(1 << SERVO_PIN);
     center_servo();
+    time_until_rising_edge_us = 0;
+    time_until_falling_edge_us = -1;
     register_periodic_timer_ISR(0, PULSE_INCREMENT_uS, handle_timer_interrupt);
 }
 
-char *test_servo(char *buffer) {
-
-    if (!initialized) {
+char *test_servo(char *buffer)
+{
+    if (cowpi_left_button_is_pressed())
+    {
         center_servo();
-        initialized = true;
-        centered = true;
-        sprintf(buffer, "Center");
-        return buffer;
+        sprintf(buffer, "SERVO: center");
     }
-
-    if (cowpi_right_button_is_pressed()) {
-        if (!button_already_pressed) {
-            toggled = !toggled;
-            button_already_pressed = true;
-            time_until_rising_edge = 0;
-            centered = false;
+    else
+    {
+        if (cowpi_left_switch_is_in_left_position())
+        {
+            rotate_full_clockwise();
+            sprintf(buffer, "SERVO: right");
         }
-    } else {
-        button_already_pressed = false;
-    }
-
-    if (centered) {
-        center_servo();
-        sprintf(buffer, "Center");
-    } else if (toggled) {
-        rotate_full_clockwise();
-        sprintf(buffer, "Clockwise");
-    } else {
-        rotate_full_counterclockwise();
-        sprintf(buffer, "Counterclockwise");
+        else if (cowpi_left_switch_is_in_right_position())
+        {
+            rotate_full_counterclockwise();
+            sprintf(buffer, "SERVO: left");
+        }
     }
 
     return buffer;
 }
 
-
-void center_servo() {
-    pulse_width_us = 1500;
+void center_servo()
+{
+    pulse_width_us = 3 * PULSE_INCREMENT_uS;
 }
 
-void rotate_full_clockwise() {
-    pulse_width_us = 1000;
+void rotate_full_clockwise()
+{
+    pulse_width_us = PULSE_INCREMENT_uS;
 }
 
-void rotate_full_counterclockwise() {
-    pulse_width_us = 2000;
+void rotate_full_counterclockwise()
+{
+    pulse_width_us = 5 * PULSE_INCREMENT_uS;
+}
+
+static void handle_timer_interrupt()
+{
+    if (time_until_rising_edge_us <= 0)
+    {
+        digitalWrite(SERVO_PIN, 1);
+        time_until_rising_edge_us += SIGNAL_PERIOD_uS;
+        time_until_falling_edge_us = pulse_width_us;
+    }
+
+    if (time_until_falling_edge_us <= 0)
+    {
+        digitalWrite(SERVO_PIN, 0);
+    }
+
+    time_until_rising_edge_us -= PULSE_INCREMENT_uS;
+    time_until_falling_edge_us -= PULSE_INCREMENT_uS;
 }
